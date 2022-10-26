0539274cd15c75797b2092cebc90c45e6f9c7a52
obkin <kapellwork@gmail.com>
2022-10-26 09:49:07 +0300

    using_jquery

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-import 'jquery';
+import $ from 'jquery';
 
-const btn = $('#btn');
-console.log(btn);
+$(document).ready(function() {
+    $('.list-item:first').hover(function() {
+        $(this).toggleClass('active');
+    });
+});
