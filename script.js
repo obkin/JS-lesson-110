e2374dc613d12e3ece001454fde2866d87dffa5a
obkin <kapellwork@gmail.com>
2022-10-26 15:44:16 +0300

    end_of_lesson

--- script.js
+++ script.js
@@ -8,4 +8,11 @@ $(document).ready(function() {
     $('.list-item:eq(2)').on('click', () => {
         $('.image:even').fadeToggle('slow');
     });
+
+    $('.list-item:eq(4)').on('click', () => {
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle'
+        }, 2000);
+    });
 });
